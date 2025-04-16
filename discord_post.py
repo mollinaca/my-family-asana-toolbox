c457bd170ffe6fc419b955f2cb6641d73934108e
mollinaca <mail@mollinaca.dev>
2025-04-16 22:33:31 +0900

    add

---
+++ discord_post.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import datetime
+import os
+import sys
+import requests
+#from dotenv import load_dotenv
+#load_dotenv()  # take environment variables
+
+def main():
+    WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
+
+    if len(sys.argv) > 1:
+        message = " ".join(sys.argv[1:])
+    else:
+        message = DEFAULT_MESSAGE
+
+    payload = {
+        "content": message
+    }
+
+    response = requests.post(WEBHOOK_URL, json=payload)
+
+    if response.status_code == 204:
+        print("message post successes")
+    else:
+        print(f"message post failed, status_code: {response.status_code}")
+
+if __name__ == '__main__':
+    now_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    DEFAULT_MESSAGE = f"test from discord_post.py : {now_str}"
+    main()
