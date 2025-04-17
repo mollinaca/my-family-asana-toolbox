dd9e88f109e121b103909042024d23b1b685cb42
mollinaca <mail@mollinaca.dev>
2025-04-17 22:17:59 +0900

    add

---
+++ test_script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+import os
+import asana_api
+from dotenv import load_dotenv
+load_dotenv()
+
+def main():
+    print ("test_script.py !")
+
+if __name__ == '__main__':
+    main()
