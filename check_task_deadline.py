0643efb1afd75ca6010162820a4cb9d8b29c0944
mollinaca <mail@mollinaca.dev>
2025-05-06 21:00:57 +0900

    期日判定のバグ修正

--- check_task_deadline.py
+++ check_task_deadline.py
@@ -5,9 +5,8 @@ import discord_post
 
 def main():
     deadline_task_found = False # 期日が近いタスクが発見されたら True にする
-    answer = [] # 期日が近いタスクの uri を入れるリスト ("name","permalink_url") のタプルを一つの要素として入れる
+    answer = [] # 期日が近いタスクの uri を入れるリスト ("name : due_on","permalink_url") のタプルを一つの要素として入れる
     today = datetime.today().date()
-    one_week_ago = today - timedelta(days=7)
 
     a = asana_functions.AsanaFunctions()
     task_gid_list = a.get_all_tasks()
@@ -22,18 +21,19 @@ def main():
             # 通知
             exit (1)
         else:
-            res = res["response"]
+            res = res["response"]["response"]
 
-        due_on = res["response"]["due_on"]
+        due_on = res["due_on"]
 
         if due_on is None:
             continue
 
-        date_obj = datetime.strptime(due_on, "%Y-%m-%d").date()
+        date_deadline = datetime.strptime(due_on, "%Y-%m-%d").date()
+        week_before_due = date_deadline - timedelta(days=7)
 
-        if one_week_ago <= date_obj <= today:
+        if week_before_due <= today <= date_deadline:
             deadline_task_found = True
-            answer.append((res["name"], res["permalink_url"]))
+            answer.append((f"{res["name"]} : {due_on}", res["permalink_url"]))
 
     if deadline_task_found:
         message = "期日が近いタスクがあります。\n\n"
