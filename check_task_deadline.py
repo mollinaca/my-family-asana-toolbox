f30e4491b15328d03da66d08caae4a21efc4424d
mollinaca <mail@mollinaca.dev>
2025-05-10 22:16:25 +0900

    update 期日を過ぎたタスクを通知するように調整

--- check_task_deadline.py
+++ check_task_deadline.py
@@ -4,8 +4,8 @@ import asana_functions
 import discord_post
 
 def main():
-    deadline_task_found = False # 期日が近いタスクが発見されたら True にする
-    answer = [] # 期日が近いタスクの uri を入れるリスト ("name : due_on","permalink_url") のタプルを一つの要素として入れる
+    answer1 = [] # 期日が近いタスクの uri を入れるリスト ("name : due_on","permalink_url") のタプルを一つの要素として入れる
+    answer2 = [] # 期日が過ぎたタスクの uri を入れるリスト ("name : due_on","permalink_url") のタプルを一つの要素として入れる
     today = datetime.today().date()
 
     a = asana_functions.AsanaFunctions()
@@ -32,18 +32,28 @@ def main():
         week_before_due = date_deadline - timedelta(days=7)
 
         if week_before_due <= today <= date_deadline:
-            deadline_task_found = True
-            answer.append((f"{res["name"]} : {due_on}", res["permalink_url"]))
+            answer1.append((f"{res["name"]} : {due_on}", res["permalink_url"]))
+        
+        if date_deadline < today:
+            answer2.append((f"{res["name"]} : {due_on}", res["permalink_url"]))
 
-    if deadline_task_found:
+    if answer1:
         message = "期日が近いタスクがあります。\n\n"
-        message += "\n\n".join(f"{x}\n{y}" for x, y in answer)
+        message += "\n\n".join(f"{x}\n{y}" for x, y in answer1)
         discord_post.post(message)
         print ("check_task_deadline completed")
-        exit (0)
-    else:
+
+    if answer2:
+        message = "期日が過ぎたタスクがあります。\n\n"
+        message += "\n\n".join(f"{x}\n{y}" for x, y in answer2)
+        discord_post.post(message)
+        print ("check_task_deadline completed")
+
+
+    if not answer1 and not answer2:
         print ("no deadline task found, exit script.")
-        exit (0)
+
+    exit (0)
 
 if __name__ == '__main__':
     main()
